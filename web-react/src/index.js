05104daf7a8f6a3f1c597550de02b654dcda0c62
Win@L <win@phuket.net>
2019-10-08 16:00:58 +0700

    update to current.

--- web-react/src/index.js
+++ web-react/src/index.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import './resources/StyleSheet/index.css'; 
+import './resources/StyleSheet/index.scss'; 
 import 'bulma/css/bulma.css';
 import Container from './pages/Container.react';
- 
+import createStore from './store/createStore'
+import { Provider } from 'react-redux'
  
 const app = document.getElementById('root');
 
-import createStore from './store/createStore'
-import { Provider } from 'react-redux'
 
 const store = createStore()
 
